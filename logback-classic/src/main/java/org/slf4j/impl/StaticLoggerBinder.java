27db1b9fc00e997548ec08545ca7950ccc1cf2ca
chengtf94 <chengtf94@qq.com>
2024-05-01 16:18:31 +0800

    read

--- logback-classic/src/main/java/org/slf4j/impl/StaticLoggerBinder.java
+++ logback-classic/src/main/java/org/slf4j/impl/StaticLoggerBinder.java
@@ -1,16 +1,3 @@
-/**
- * Logback: the reliable, generic, fast and flexible logging framework.
- * Copyright (C) 1999-2015, QOS.ch. All rights reserved.
- *
- * This program and the accompanying materials are dual-licensed under
- * either the terms of the Eclipse Public License v1.0 as published by
- * the Eclipse Foundation
- *
- *   or (per the licensee's choosing)
- *
- * under the terms of the GNU Lesser General Public License version 2.1
- * as published by the Free Software Foundation.
- */
 package org.slf4j.impl;
 
 import ch.qos.logback.core.status.StatusUtil;
@@ -34,50 +21,14 @@ import ch.qos.logback.core.util.StatusPrinter;
  * @author Ceki G&uuml;lc&uuml;</a>
  */
 public class StaticLoggerBinder implements LoggerFactoryBinder {
-
-    /**
-     * Declare the version of the SLF4J API this implementation is compiled
-     * against. The value of this field is usually modified with each release.
-     */
-    // to avoid constant folding by the compiler, this field must *not* be final
-    public static String REQUESTED_API_VERSION = "1.7.16"; // !final
-
-    final static String NULL_CS_URL = CoreConstants.CODES_URL + "#null_CS";
-
-    /**
-     * The unique instance of this class.
-     */
     private static StaticLoggerBinder SINGLETON = new StaticLoggerBinder();
-
-    private static Object KEY = new Object();
-
-    static {
-        SINGLETON.init();
-    }
-
-    private boolean initialized = false;
-    private LoggerContext defaultLoggerContext = new LoggerContext();
-    private final ContextSelectorStaticBinder contextSelectorBinder = ContextSelectorStaticBinder.getSingleton();
-
-    private StaticLoggerBinder() {
-        defaultLoggerContext.setName(CoreConstants.DEFAULT_CONTEXT_NAME);
-    }
-
     public static StaticLoggerBinder getSingleton() {
         return SINGLETON;
     }
-
-    /**
-     * Package access for testing purposes.
-     */
-    static void reset() {
-        SINGLETON = new StaticLoggerBinder();
+    private LoggerContext defaultLoggerContext = new LoggerContext();
+    static {
         SINGLETON.init();
     }
-
-    /**
-     * Package access for testing purposes.
-     */
     void init() {
         try {
             try {
@@ -96,17 +47,52 @@ public class StaticLoggerBinder implements LoggerFactoryBinder {
         }
     }
 
+    @Override
     public ILoggerFactory getLoggerFactory() {
         if (!initialized) {
             return defaultLoggerContext;
         }
-
         if (contextSelectorBinder.getContextSelector() == null) {
             throw new IllegalStateException("contextSelector cannot be null. See also " + NULL_CS_URL);
         }
         return contextSelectorBinder.getContextSelector().getLoggerContext();
     }
 
+    /**
+     * Declare the version of the SLF4J API this implementation is compiled
+     * against. The value of this field is usually modified with each release.
+     */
+    // to avoid constant folding by the compiler, this field must *not* be final
+    public static String REQUESTED_API_VERSION = "1.7.16"; // !final
+
+    final static String NULL_CS_URL = CoreConstants.CODES_URL + "#null_CS";
+
+
+    private static Object KEY = new Object();
+
+    private boolean initialized = false;
+
+    private final ContextSelectorStaticBinder contextSelectorBinder = ContextSelectorStaticBinder.getSingleton();
+
+    private StaticLoggerBinder() {
+        defaultLoggerContext.setName(CoreConstants.DEFAULT_CONTEXT_NAME);
+    }
+
+
+
+    /**
+     * Package access for testing purposes.
+     */
+    static void reset() {
+        SINGLETON = new StaticLoggerBinder();
+        SINGLETON.init();
+    }
+
+
+
+
+
+    @Override
     public String getLoggerFactoryClassStr() {
         return contextSelectorBinder.getClass().getName();
     }
